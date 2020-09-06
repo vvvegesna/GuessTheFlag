65286cdf49ee5c6c0a284edb40c061b9f954bb03
Vegesna, Vijay V EX1 <VVegesna@us.imshealth.com>
2020-09-06 06:57:12 -0700

    added accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -23,10 +23,23 @@ extension View {
 }
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var scoreTitle = ""
+    @State private var resultTitle = ""
     @State private var showingScore = false
     @State private var myScore = 0
     @State private var tappedIndex = 0
@@ -46,12 +59,14 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .fontWeight(.black)
                 }
+                .accessibility(label: Text("Tap the flag of \(countries[correctAnswer])"))
                 ForEach (0..<3) { number in
                     Button(action: {
                         self.flagTapped(number)
                     }) {
                         Image(self.countries[number]).renderingMode(.original)
                         .setFlagView()
+                            .accessibility(label: Text(self.labels[self.countries[number], default: "Unknown flag"]))
                         .rotation3DEffect(.degrees(self.spinAnimation), axis: (x: 0, y: 1, z: 0))
                     }
                 }
@@ -67,7 +82,7 @@ struct ContentView: View {
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("That's the flag of \(countries[tappedIndex])"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(resultTitle), message: Text("That's the flag of \(countries[tappedIndex])"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
                 })
         }
@@ -75,7 +90,7 @@ struct ContentView: View {
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            resultTitle = "Correct"
             myScore += 1
             tappedIndex = correctAnswer
             withAnimation(.easeInOut(duration: 2)){
@@ -83,7 +98,7 @@ struct ContentView: View {
             }
         } else {
             tappedIndex = number
-            scoreTitle = "Wrong"
+            resultTitle = "Wrong"
             myScore -= 1
         }
         showingScore = true
